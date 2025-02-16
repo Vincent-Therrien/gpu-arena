43fa9c20ef0dcdb859d109f3d503d49a3c79cedd
Vincent Therrien <vincentstherrien@gmail.com>
2025-02-15 19:38:25 -0500

    v24

--- webgpu/graphics/src/main.rs
+++ webgpu/graphics/src/main.rs
@@ -1,36 +1,298 @@
-use winit::{
-    event::*,
-    event_loop::EventLoop,
-    keyboard::{KeyCode, PhysicalKey},
-    window::WindowBuilder,
-};
+use glfw::{fail_on_errors, Action, Key, Window, WindowHint, ClientApiHint};
 
-pub fn run() {
-    env_logger::init();
-    let event_loop = EventLoop::new().unwrap();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+use std::env::current_dir;
+use std::fs;
 
-    event_loop.run(move |event, control_flow| match event {
-        Event::WindowEvent {
-            ref event,
-            window_id,
-        } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested
-            | WindowEvent::KeyboardInput {
-                event:
-                    KeyEvent {
-                        state: ElementState::Pressed,
-                        physical_key: PhysicalKey::Code(KeyCode::Escape),
-                        ..
-                    },
-                ..
-            } => control_flow.exit(),
-            _ => {}
-        },
-        _ => {}
-    });
+pub struct PipelineBuilder {
+    shader_filename: String,
+    vertex_entry: String,
+    fragment_entry: String,
+    pixel_format: wgpu::TextureFormat,
+}
+
+impl PipelineBuilder {
+
+    pub fn new() -> Self {
+        PipelineBuilder {
+            shader_filename: "dummy".to_string(),
+            vertex_entry: "dummy".to_string(),
+            fragment_entry: "dummy".to_string(),
+            pixel_format: wgpu::TextureFormat::Rgba8Unorm,
+        }
+    }
+
+    pub fn set_shader_module(&mut self, shader_filename: &str, vertex_entry: &str, fragment_entry: &str) {
+
+        self.shader_filename = shader_filename.to_string();
+        self.vertex_entry = vertex_entry.to_string();
+        self.fragment_entry = fragment_entry.to_string();
+    }
+
+    pub fn set_pixel_format(&mut self, pixel_format: wgpu::TextureFormat) {
+
+        self.pixel_format = pixel_format;
+    }
+
+    pub fn build_pipeline(&self, device: &wgpu::Device) -> wgpu::RenderPipeline {
+
+        let mut filepath = current_dir().unwrap();
+        filepath.push("src/");
+        filepath.push(self.shader_filename.as_str());
+        let filepath = filepath.into_os_string().into_string().unwrap();
+        let source_code = fs::read_to_string(filepath).expect("Can't read source code!");
+
+        let shader_module_descriptor = wgpu::ShaderModuleDescriptor {
+            label: Some("Shader Module"),
+            source: wgpu::ShaderSource::Wgsl(source_code.into()),
+        };
+        let shader_module = device.create_shader_module(shader_module_descriptor);
+
+        let pipeline_layout_descriptor = wgpu::PipelineLayoutDescriptor {
+            label: Some("Render Pipeline Layout"),
+            bind_group_layouts: &[],
+            push_constant_ranges: &[],
+        };
+        let pipeline_layout = device.create_pipeline_layout(&pipeline_layout_descriptor);
+
+        let render_targets = [Some(wgpu::ColorTargetState {
+            format: self.pixel_format,
+            blend: Some(wgpu::BlendState::REPLACE),
+            write_mask: wgpu::ColorWrites::ALL,
+        })];
+
+        let render_pipeline_descriptor = wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&pipeline_layout),
+
+            vertex: wgpu::VertexState {
+                module: &shader_module,
+                entry_point: Some(&self.vertex_entry),
+                buffers: &[],
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
+            },
+
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+
+            fragment: Some(wgpu::FragmentState {
+                module: &shader_module,
+                entry_point: Some(&self.fragment_entry),
+                targets: &render_targets,
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
+            }),
+
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+            cache: None,
+        };
+
+        device.create_render_pipeline(&render_pipeline_descriptor)
+    }
+}
+
+struct State<'a> {
+    instance: wgpu::Instance,
+    surface: wgpu::Surface<'a>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: (i32, i32),
+    window: &'a mut Window,
+    render_pipeline: wgpu::RenderPipeline,
+}
+
+impl<'a> State<'a> {
+
+    async fn new(window: &'a mut Window) -> Self {
+
+        let size = window.get_framebuffer_size();
+
+        let instance_descriptor = wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(), ..Default::default()
+        };
+        let instance = wgpu::Instance::new(&instance_descriptor);
+        let surface = instance.create_surface(window.render_context()).unwrap();
+
+        let adapter_descriptor = wgpu::RequestAdapterOptionsBase {
+            power_preference: wgpu::PowerPreference::default(),
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false,
+        };
+        let adapter = instance.request_adapter(&adapter_descriptor)
+            .await.unwrap();
+
+        let device_descriptor = wgpu::DeviceDescriptor {
+            required_features: wgpu::Features::empty(),
+            required_limits: wgpu::Limits::default(),
+            label: Some("Device"),
+            memory_hints: wgpu::MemoryHints::default(),
+        };
+        let (device, queue) = adapter
+            .request_device(&device_descriptor, None)
+            .await.unwrap();
+
+
+        let surface_capabilities = surface.get_capabilities(&adapter);
+        let surface_format = surface_capabilities
+            .formats
+            .iter()
+            .copied()
+            .filter(|f | f.is_srgb())
+            .next()
+            .unwrap_or(surface_capabilities.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.0 as u32,
+            height: size.1 as u32,
+            present_mode: surface_capabilities.present_modes[0],
+            alpha_mode: surface_capabilities.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2
+        };
+        surface.configure(&device, &config);
+
+        let mut pipeline_builder = PipelineBuilder::new();
+        pipeline_builder.set_shader_module("shader.wgsl", "vs_main", "fs_main");
+        pipeline_builder.set_pixel_format(config.format);
+        let render_pipeline = pipeline_builder.build_pipeline(&device);
+
+        Self {
+            instance,
+            window,
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            render_pipeline,
+        }
+    }
+
+    fn resize(&mut self, new_size: (i32, i32)) {
+        if new_size.0 > 0 && new_size.1 > 0 {
+            self.size = new_size;
+            self.config.width = new_size.0 as u32;
+            self.config.height = new_size.1 as u32;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    fn update_surface(&mut self) {
+        self.surface = self.instance.create_surface(self.window.render_context()).unwrap();
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError>{
+
+        let drawable = self.surface.get_current_texture()?;
+        let image_view_descriptor = wgpu::TextureViewDescriptor::default();
+        let image_view = drawable.texture.create_view(&image_view_descriptor);
+
+        let command_encoder_descriptor = wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder")
+        };
+        let mut command_encoder = self.device.create_command_encoder(&command_encoder_descriptor);
+
+        let color_attachment = wgpu::RenderPassColorAttachment {
+            view: &image_view,
+            resolve_target: None,
+            ops: wgpu::Operations {
+                load: wgpu::LoadOp::Clear(wgpu::Color {
+                    r: 0.75,
+                    g: 0.5,
+                    b: 0.25,
+                    a: 1.0
+                }),
+                store: wgpu::StoreOp::Store,
+            },
+        };
+
+        let render_pass_descriptor = wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[Some(color_attachment)],
+            depth_stencil_attachment: None,
+            occlusion_query_set: None,
+            timestamp_writes: None
+        };
+
+        {
+            let mut renderpass = command_encoder.begin_render_pass(&render_pass_descriptor);
+            renderpass.set_pipeline(&self.render_pipeline);
+            renderpass.draw(0..3, 0..1);
+        }
+        self.queue.submit(std::iter::once(command_encoder.finish()));
+
+        drawable.present();
+
+        Ok(())
+    }
+}
+
+async fn run() {
+
+    let mut glfw = glfw::init(fail_on_errors!())
+        .unwrap();
+    glfw.window_hint(WindowHint::ClientApi(ClientApiHint::NoApi));
+    let (mut window, events) =
+        glfw.create_window(
+            800, 600, "It's WGPU time.",
+            glfw::WindowMode::Windowed).unwrap();
+
+    let mut state = State::new(&mut window).await;
+
+    state.window.set_framebuffer_size_polling(true);
+    state.window.set_key_polling(true);
+    state.window.set_mouse_button_polling(true);
+    state.window.set_pos_polling(true);
+
+    while !state.window.should_close() {
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            match event {
+
+                //Hit escape
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    state.window.set_should_close(true)
+                }
+
+                //Window was moved
+                glfw::WindowEvent::Pos(..) => {
+                    state.update_surface();
+                    state.resize(state.size);
+                }
+
+                //Window was resized
+                glfw::WindowEvent::FramebufferSize(width, height) => {
+                    state.update_surface();
+                    state.resize((width, height));
+                }
+                _ => {}
+            }
+        }
+
+        match state.render() {
+            Ok(_) => {},
+            Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                state.update_surface();
+                state.resize(state.size);
+            },
+            Err(e) => eprintln!("{:?}", e),
+        }
+    }
 }
 
 fn main() {
-    run();
+    pollster::block_on(run());
 }
